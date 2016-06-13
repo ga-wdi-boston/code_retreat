52848776fbdf08ddca6565fc016092dd562b3b01
MishaHerscu <mishaherscu@gmail.com>
2016-06-13 16:34:56 -0400

    fixed linter

--- lib/conway.rb
+++ lib/conway.rb
@@ -12,39 +12,39 @@ module Game
     end
 
     def initial_populate
-      board_array.map! { |index| board_array[index] = rand.round }
+      board_array.map! { |index| board_array[index] = 1 }
     end
 
-    def tick
+    def tick(iterator)
       tmp = board_array
       tmp.map! { |index| tmp[index] = update(index, board_array) }
       board_array = tmp
-      p board_array
+      p "Iteration: #{iterator}: #{board_array}"
     end
 
-    # def neighbors(cell, width)
-    #   values = board.slice((board.index(cell) - width - 1), 3).reduce(:+) +
-    #   gameboard.slice((gameboard.index(cell) + width - 1), 3).reduce(:+) +
-    #   gameboard.slice((gameboard.index(cell) - 1), 3).reduce(:+)
-    #   values
+    def neighbors_count(index)
+      @board_array.slice(index - @width - 1, 3).reduce(:+) +
+        @board_array[index - 1] +
+        @board_array[index + 1] +
+        @board_array.slice(index + @width - 1, 3).reduce(:+)
+    end
+
+    # def build_neighbors_array(index)
+    #   neighbors_array = [@board_array[index - @width - 1],
+    #                      @board_array[index - @width],
+    #                      @board_array[index - @width + 1],
+    #                      @board_array[index - 1],
+    #                      @board_array[index + 1],
+    #                      @board_array[index + @width - 1],
+    #                      @board_array[index + @width],
+    #                      @board_array[index + @width + 1]]
+    #   final_neighbors_array = neighbors_array.compact
+    #   final_neighbors_array
     # end
-
-    def build_neighbors_array(index)
-      neighbors_array = []
-      neighbors_array.push(@board_array[index - @width - 1])
-      neighbors_array.push(@board_array[index - @width])
-      neighbors_array.push(@board_array[index - @width + 1])
-      neighbors_array.push(@board_array[index - 1])
-      neighbors_array.push(@board_array[index + 1])
-      neighbors_array.push(@board_array[index + @width - 1])
-      neighbors_array.push(@board_array[index + @width])
-      neighbors_array.push(@board_array[index + @width + 1])
-      neighbors_array.compact!
-    end
-
-    def reduce_neighbors_array(neighbors_array)
-      neighbors_array.reduce(:+)
-    end
+    #
+    # def reduce_neighbors_array(neighbors_array)
+    #   neighbors_array.reduce(:+)
+    # end
 
     def live_or_die(status, sum)
       if (status == 0 && sum == 3) || (status == 1 && (sum == 2 || sum == 3))
@@ -55,8 +55,7 @@ module Game
     end
 
     def update(index, array)
-      live_or_die(array[index],
-                  reduce_neighbors_array(build_neighbors_array(index)))
+      live_or_die(array[index], neighbors_count(index))
     end
   end
 end
@@ -64,5 +63,5 @@ end
 # tests
 sample_game = Game::Board.new(4, 4)
 p sample_game
-sample_game.tick
-sample_game.tick
+counter = 1.upto(10)
+counter.each { |iterator| sample_game.tick(iterator) }
