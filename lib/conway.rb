8c96274611b03f36e2307a32115d2f89f81b18fb
MishaHerscu <mishaherscu@gmail.com>
2016-06-13 16:58:39 -0400

    print boards

--- lib/conway.rb
+++ lib/conway.rb
@@ -3,6 +3,8 @@ module Game
   # class Board
   class Board
     attr_accessor :board_array
+    attr_reader :width, :length, :cell_count
+
     def initialize(length, width)
       @width = width
       @length = length
@@ -12,14 +14,15 @@ module Game
     end
 
     def initial_populate
-      board_array.map! { |index| board_array[index] = 1 }
+      board_array.map! { |index| board_array[index] = rand.round }
+      print_board(board_array, 0)
     end
 
     def tick(iterator)
       tmp = board_array
       tmp.map! { |index| tmp[index] = update(index, board_array) }
       board_array = tmp
-      p "Iteration: #{iterator}: #{board_array}"
+      print_board(board_array, iterator)
     end
 
     def neighbors_count(index)
@@ -40,11 +43,22 @@ module Game
     def update(index, array)
       live_or_die(array[index], neighbors_count(index))
     end
+
+    def print_board(board_array, iterator)
+      rows = []
+      row_counter = 0.upto(length - 1)
+      row_counter.each do |counter|
+        row = board_array.slice(counter, width)
+        rows.push(row)
+      end
+      p iterator
+      rows.each { |row_of_grid| p row_of_grid }
+    end
   end
 end
 
 # tests
 sample_game = Game::Board.new(4, 4)
 p sample_game
-counter = 1.upto(9)
+counter = 1.upto(4)
 counter.each { |iterator| sample_game.tick(iterator) }
